4a754771fd77eacb30da00b775666689750b6e21
Arthur Kolychev <aalkolychev@sberbank.ru>
2023-02-05 19:41:23 +0300

    abstract class AbstractStorage added + class Tester refactored

--- src/main/java/ru/sberbank/service/DeveloperService.java
+++ src/main/java/ru/sberbank/service/DeveloperService.java
@@ -4,37 +4,39 @@ import ru.sberbank.data.Developer;
 
 import java.util.ArrayList;
 
-public class DeveloperService {
-    public ArrayList<Developer> list;
+public class DeveloperService extends AbstractStorage<Developer> implements DataService<Developer>{
 
     public DeveloperService() {
-        this.list = new ArrayList<>();
+        super();
     }
 
-    public void createDeveloper(int id, String firstName, String secondName) throws IllegalStateException {
+    @Override
+    public void create(int id, String firstName, String secondName) {
         if((firstName == null || firstName.isEmpty()) || (secondName == null || secondName.isEmpty())) {
             throw new IllegalStateException("Входные данные не валидны");
         }
         list.add(new Developer(id, firstName, secondName));
     }
 
-    public Developer getDeveloper(String firstName, String secondName) throws IllegalStateException {
+    @Override
+    public Developer get(String firstName, String secondName) {
         for (Developer t: list)  {
-            if(firstName.equalsIgnoreCase(t.firstName) && secondName.equalsIgnoreCase(t.secondName)) {
+            if(firstName.equalsIgnoreCase(t.getFirstName()) && secondName.equalsIgnoreCase(t.getSecondName())) {
                 return t;
             }
         }
         throw new IllegalStateException("Разработчики не найдены");
     }
 
-    public ArrayList<Developer> getFreeDevelopers() {
-        ArrayList<Developer> freeTester = new ArrayList<>();
+    @Override
+    public ArrayList<Developer> getListOfFree() {
+        ArrayList<Developer> freeDeveloper = new ArrayList<>();
         for (Developer n: list) {
-            if(n.isFree) {
-                freeTester.add(n);
+            if(n.isFree()) {
+                freeDeveloper.add(n);
             }
         }
-        return freeTester;
+        return freeDeveloper;
     }
 
 }
