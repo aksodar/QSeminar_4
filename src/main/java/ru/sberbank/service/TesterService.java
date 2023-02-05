248bb5ecc5338aa19decefa0e82ac387c70fc8cc
Tatiana-Myshkovskaia <utkina.td7@gmail.com>
2023-02-05 20:22:03 +0300

    fix for Seminar4.5

--- src/main/java/ru/sberbank/service/TesterService.java
+++ src/main/java/ru/sberbank/service/TesterService.java
@@ -1,24 +1,27 @@
 package ru.sberbank.service;
 
+import ru.sberbank.data.Developer;
 import ru.sberbank.data.Tester;
 import java.util.ArrayList;
 
-public class TesterService extends AbstractStorage<Tester> {
+public class TesterService extends AbstractStorage<Tester> implements DataService<Tester> {
     public ArrayList<Tester> list;
 
     public TesterService() {
         super();
     }
 
-    public void createTester(int id, String firstName, String secondName){
+    @Override
+    public boolean create(int id, String firstName, String secondName) {
         if (firstName == null || secondName == null || firstName == "" || secondName == ""){
             throw new IllegalStateException("Tester is not created");
         }
         Tester QA = new Tester(id, firstName, secondName);
-        list.add(QA);
+        return list.add(QA);
     }
 
-    public Tester getTester(String firstName, String secondName) {
+    @Override
+    public Tester get(String firstName, String secondName) {
         for (Tester n : list) {
             if (firstName.equalsIgnoreCase(n.getFirstName()) && secondName.equalsIgnoreCase(n.getSecondName()))
                 return n;
@@ -27,7 +30,8 @@ public class TesterService extends AbstractStorage<Tester> {
     }
 
 
-    public ArrayList getFreeTesters() {
+    @Override
+    public ArrayList<Tester> getListOfFree() {
         ArrayList<Tester> temp = new ArrayList<>();
         for (Tester n : list) {
             if (n.isFree()) {
@@ -39,7 +43,17 @@ public class TesterService extends AbstractStorage<Tester> {
         }
         return temp;
     }
-}
 
-
+    @Override
+    public Tester get(int id) {
+        for (Tester a:list) {
+            if(a != null){
+                return a;
+            }
+        }
+        throw new IllegalStateException("Tester is not found");
+    }
+        // дописать этот метод с учетом перегрузки get
+    // не уверена, что здесь должен быть список, но по идее хочу перебрать весь список Тестеров, у которых id is not null (возможно ли такое?)
+    }
 
