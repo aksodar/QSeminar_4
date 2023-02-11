a8b942fd9572dbb71c5cf4e4ee0f1629c375cbe6
Aleksandr Davydov <av.davydof@gmail.com>
2023-02-11 19:36:42 +0700

    + hw of 5 day

--- src/main/java/ru/sberbank/service/DeveloperService.java
+++ src/main/java/ru/sberbank/service/DeveloperService.java
@@ -4,20 +4,22 @@ import ru.sberbank.data.Developer;
 
 import java.util.ArrayList;
 
-public class DeveloperService extends AbstractStorage<Developer> {
+public class DeveloperService extends AbstractStorage<Developer> implements DataService<Developer> {
 
     public DeveloperService() {
         super();
     }
 
-    public void createDeveloper(int id, String firstName, String secondName) throws IllegalStateException {
+    @Override
+    public void create(int id, String firstName, String secondName) {
         if ((firstName == null || firstName.isEmpty()) || (secondName == null || secondName.isEmpty())) {
             throw new IllegalStateException("Входные данные не валидны");
         }
         list.add(new Developer(id, firstName, secondName));
     }
 
-    public Developer getDeveloper(String firstName, String secondName) throws IllegalStateException {
+    @Override
+    public Developer get(String firstName, String secondName) {
         for (Developer t : list) {
             if (firstName.equalsIgnoreCase(t.getFirstName()) && secondName.equalsIgnoreCase(t.getSecondName())) {
                 return t;
@@ -26,7 +28,18 @@ public class DeveloperService extends AbstractStorage<Developer> {
         throw new IllegalStateException("Разработчики не найдены");
     }
 
-    public ArrayList<Developer> getFreeDevelopers() {
+    @Override
+    public Developer get(int id) {
+        for (Developer t : list) {
+            if (id == t.getId()) {
+                return t;
+            }
+        }
+        throw new IllegalStateException("Разработчики не найдены");
+    }
+
+    @Override
+    public ArrayList<Developer> getListOfFree() {
         ArrayList<Developer> freeTester = new ArrayList<>();
         for (Developer n : list) {
             if (n.isFree()) {
@@ -35,5 +48,4 @@ public class DeveloperService extends AbstractStorage<Developer> {
         }
         return freeTester;
     }
-
 }
--- src/main/java/ru/sberbank/service/TesterService.java
+++ src/main/java/ru/sberbank/service/TesterService.java
@@ -1,16 +1,18 @@
 package ru.sberbank.service;
 
+import ru.sberbank.data.TeamMember;
 import ru.sberbank.data.Tester;
 
 import java.util.ArrayList;
 
-public class TesterService extends AbstractStorage<Tester> {
+public class TesterService extends AbstractStorage<Tester> implements DataService<Tester> {
 
     public TesterService() {
         super();
     }
 
-    public void createTester(int id, String firstName, String secondName) {
+    @Override
+    public void create(int id, String firstName, String secondName) {
 
         if (firstName == null || firstName.isEmpty()) {
             throw new IllegalArgumentException("incorrect firstname");
@@ -21,7 +23,8 @@ public class TesterService extends AbstractStorage<Tester> {
         this.list.add(new Tester(id, firstName, secondName));
     }
 
-    public Tester getTester(String firstName, String secondName) {
+    @Override
+    public Tester get(String firstName, String secondName) {
 
         for (Tester tester : this.list) {
             if (tester.getFirstName().equalsIgnoreCase(firstName)
@@ -30,9 +33,22 @@ public class TesterService extends AbstractStorage<Tester> {
             }
         }
         throw new IllegalArgumentException("Tester not found");
+
     }
 
-    public ArrayList<Tester> getFreeTesters() {
+    @Override
+    public Tester get(int id) {
+
+        for (Tester tester : this.list) {
+            if (id == tester.getId()) {
+                return tester;
+            }
+        }
+        throw new IllegalArgumentException("Tester not found");
+    }
+
+    @Override
+    public ArrayList<Tester> getListOfFree() {
         ArrayList<Tester> freeTesters = new ArrayList<>();
         for (Tester tester : this.list) {
             if (tester.isFree()) {
@@ -41,5 +57,6 @@ public class TesterService extends AbstractStorage<Tester> {
         }
         return freeTesters;
     }
+
 
 }
